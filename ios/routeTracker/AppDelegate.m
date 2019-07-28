a024613eeb8cdb375e8fb5a6d097de343e3d249f
shubhanu <shubhanu@cartoonmango.com>
2019-07-28 13:03:26 +0530

    Pod installed

--- ios/routeTracker/AppDelegate.m
+++ ios/routeTracker/AppDelegate.m
@@ -10,11 +10,13 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyAHHRDWOqrlm1CBWr_l4on8nbU3LDtFgls"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"routeTracker"
