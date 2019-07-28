ea5120be67ccf88b0ed45c2df0032c7bcc5767a2
shubhanu <shubhanu@cartoonmango.com>
2019-07-28 18:54:35 +0530

    Android working

--- android/app/src/main/java/com/routetracker/MainApplication.java
+++ android/app/src/main/java/com/routetracker/MainApplication.java
@@ -3,12 +3,13 @@ package com.routetracker;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.airbnb.android.react.maps.MapsPackage;
+import com.wix.interactable.Interactable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,8 +27,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new MapsPackage(),
-          new VectorIconsPackage()
+            new MapsPackage(),
+          // new MapsPackage(),
+          new VectorIconsPackage(),
+          new Interactable()
       );
     }
 
